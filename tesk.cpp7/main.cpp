fd07b5c1114c3610ab41cef21f5d1ac64f017387
zengkaiwei2018 <1912207369@qq,com>
2019-07-03 20:22:16 +0800

    Initial Commit

---
+++ tesk.cpp7/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tesk.cpp7
+//
+//  Created by s20181105306 on 2019/7/3.
+//  Copyright © 2019 s20181105306. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
